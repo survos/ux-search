378911447fffc5a0c0ae4c680ed9487098aff614
Tac Tacelosky <tacman@gmail.com>
2026-06-06 13:45:11 -0400

    rename to tacman

--- src/MezcalitoUxSearchBundle.php
+++ src/MezcalitoUxSearchBundle.php
@@ -104,7 +104,7 @@ class MezcalitoUxSearchBundle extends AbstractBundle
             $builder->prependExtensionConfig('framework', [
                 'asset_mapper' => [
                     'paths' => [
-                        __DIR__.'/../assets/dist' => '@mezcalito/ux-search',
+                        __DIR__.'/../assets/dist' => '@tacman/ux-search',
                     ],
                 ],
             ]);
